13c6171967d41e09cffc9f752395bdbe8db02367
User <User@DESKTOP-TC3M5R1>
2025-09-10 12:15:10 +0530

    Spring boot data jpa using mysql external database s/w

---
+++ BootProj29-SpringDataJPA-MySql/src/main/java/com/nt/runners/JobSeekerRunner.java
@@ -0,0 +1,24 @@
+package com.nt.runners;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.nt.entity.JobSeeker;
+import com.nt.service.IJobSeekerService;
+
+@Component
+public class JobSeekerRunner implements CommandLineRunner {
+	
+	@Autowired
+	private IJobSeekerService seeker;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("JobSeekerRunner.run()");
+		JobSeeker job= new JobSeeker("chanchal","hyd","BE(CSE)",90000.0f);
+		String msg = seeker.regiterJobSeeker(job);
+		System.out.println(msg);
+	}
+
+}
---
+++ BootProj29-SpringDataJPA-MySql/src/main/java/com/nt/service/IJobSeekerService.java
@@ -0,0 +1,9 @@
+package com.nt.service;
+
+import com.nt.entity.JobSeeker;
+
+public interface IJobSeekerService {
+	
+	public String regiterJobSeeker(JobSeeker seeker);
+
+}
